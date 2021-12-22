958832d2d40205b85a400a5f75186dad1f11f89d
BrianStritch <brians12ca18@gmail.com>
2021-12-22 00:50:06 +0000

    add map content to file

--- map.js
+++ map.js
@@ -5,16 +5,31 @@
  */
 
 // Using a for loop
+let nums = [1,2,3,4,5];
+let results = [];
+for (num of nums){
+    results.push(num * 2);
+}
 
+console.log(results)
 
 // Using map()
+const multiplyBy2 = function(num){
+    return num*2;
+}
+const mapResults = nums.map(multiplyBy2);
+console.log(mapResults)
 
 
 // Simplified w/ map()
+// the map function expects a function so we can pass in an un named function
+const mapResults2 = nums.map(function(num){return num*2;});
+console.log('map-results-2 =', mapResults2)
 
 
 // Simplfied w/ map() + arrow function
-
+const mapResults3 = nums.map(num => num*2);
+console.log('map-results-3 =', mapResults3)
 
 // With objects:
 const students = [
@@ -37,3 +52,11 @@ const students = [
       skill: 'CSS'
     },
   ];
+
+const studentsWithIds = students.map(student => [student.name,student.id]);
+console.log('students with ids =', studentsWithIds)
+
+// need to find out how to take key value pair for name out of the above objects
+
+const studentsWithIds2 = students.map(student => );
+console.log('students with ids 2 =', studentsWithIds2)
