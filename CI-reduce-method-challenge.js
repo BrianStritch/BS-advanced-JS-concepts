cf00c2dc563aa89473e0b24d38c0050d8afd19ef
BrianStritch <brians12ca18@gmail.com>
2021-12-30 13:44:30 +0000

    add code to CI-reduce-method-challenge

--- CI-reduce-method-challenge.js
+++ CI-reduce-method-challenge.js
@@ -41,120 +41,110 @@ let students = [
     }
 ];
 
-
-
-const highscore5 = students.reduce((acc,curr) => {
-    if(curr.results.english >= acc){
-            acc = curr.results.english;
-        }return acc;
-    },0);
-
-const biggest5 = students.reduce((acc,curr) => {
-    if(curr.results.english >= highscore5){
-        acc = `Name : ${curr.name}, Max: ${curr.results.english}`
-    }
-    return acc
-},{});
-console.log(biggest5)
-    
-// console.log('biggest =', biggest)
-// let key = curr.name;
-//     let result
-//     if (!acc[key]){
-//         if(curr.results.english >= highscore)
-//         acc[key] = curr.results.english 
-//         result =      
-//     }
-//     return result;
-//       },{});
-
-
-const biggest2 = students.reduce((acc, sub) => acc = acc > sub.results.english? acc : sub.results.english,0);
-console.log(biggest2)
-const bestInEnglish = students.filter(student => student.results.english >= biggest2);
-const highscore = bestInEnglish.map(student => {
-    for(p in student) {    
-      if (p == 'name' ) {      
-        var name = ` ${student[p]}`
-      }
-        };
-    for(g in student.results) {
-      if (g == 'english') {      
-          var max = `${student.results[g]}`
-        }
-          }; 
-       
-      return {name,max}
-      });  
-  console.log(highscore)
-
 //   Create a variable named biggest using the keyword const
 //   Assign it a value from using the reduce method on the students array
 //   Use either an arrow function inside the reduce method, or create a function and pass it into the reduce method
 //   Use a default value with the reduce method
 //   log out the variable biggest to see the value
+  /**
+   * code below given by another student
+   */
+
+const biggest = students.reduce((acc, curr) => {
+    let grade = curr.results.english;
+    if(grade > acc.max){
+        acc.name = curr.name;
+        acc.max = grade;
+    }return acc;
+}, {name: 'placeholder', max: 0});
+console.log('Students answer found on slack',biggest);
+
+
+// example of max accumulator below 
+
+const numbers = [ 1 , 2 , 3 , 4 , 8 , 5 ,6]
+
+const maxNum = numbers.reduce((max,num) =>{
+    if(num > max){
+        max = num;
+    }return max
+},0)
+
+console.log('maxNum =',maxNum)
+
+/**
+ * example below gives you the value of the person object with the highest age
+ */
+
+const people = [
+    {name: 'j', age : 8},
+    {name: 'p', age : 22},
+    {name: 'q', age : 12},
+    {name: 'r', age : 20},   
+]
+
+const maxName = people.reduce((maxAge,person) =>{
+    let age = person.age
+    if (age > maxAge.age){        
+        maxAge.age = age
+        maxAge.name = person.name
+    }return maxAge;
+},{name: '', age:0});
+console.log('maxAge =',maxName)
+
+/**
+ * example below gives you the value of the person object with the lowest age, 
+ * i set the initial age value to 1000 as it would be higher than any expected age
+ */
+
+ const peopleMin = [
+    {name: 'j', age : 8},
+    {name: 'p', age : 22},
+    {name: 'q', age : 12},
+    {name: 'r', age : 20},   
+]
+
+const minName = peopleMin.reduce((minAge,person) =>{
+    let age = person.age
+    if (age < minAge.age){        
+        minAge.age = age
+        minAge.name = person.name
+    }return minAge;
+},{name: '', age:1000});
+console.log('minAge = ',minName)
+
+
+
+/**
+ * THIS IS HOW THE CI-REDUCE-METHOD-CHALLENGE WORKS BELOW
+ */
+const highestEnglishScore = students.reduce((highestScore,student) =>{
+    let score = student.results.english // to establish the value of the individual students english score
+    //console.log('score =',score) // logs the value of score to the console for me to see
+    if(score > highestScore.max){ //checks if the current english maths result is higher than the max value so far
+        highestScore.max = score;  // sets the max: value of the initial value  to be returned
+        highestScore.name = student.name // sets the name: value to be returned
+    }return highestScore //returns the accumulator named highestScore
+},{name:'', max:0})  // initial value set to the skeleton of the object we want returned
+
+console.log('highestEnglishScore', highestEnglishScore)  // logs the returned value of highestEnglishScore to the console
+
+/**
+ * THIS returns the lowest score in english
+ */
+ const lowestEnglishScore = students.reduce((lowestScore,student) =>{
+    let score = student.results.english // to establish the value of the individual students english score
+    //console.log('score =',score) // logs the value of score to the console for me to see
+    if(score < lowestScore.result){ //checks if the current english maths result is lower than the min value so far
+        lowestScore.result = score;  // sets the min: value of the initial value  to be returned
+        lowestScore.name = student.name // sets the name: value to be returned
+    }return lowestScore //returns the accumulator named lowestScore
+},{name:'', result:100})  // initial value set to the skeleton of the object we want returned
+
+console.log('lowestEnglishScore', lowestEnglishScore)  // logs the returned value of lowestEnglishScore to the console
 
 
 
 
-// console.log(biggest)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const english = students.filter(student => student.subjects == 'english');
-// console.log(english)
-
-
-// let devs = english  // <----- change the job type here and the function below will give correct results
-
-// const mostExperience = devs.reduce((acc, dev) => acc = acc > dev.results.english? acc : dev.results.english,);
-// const hasBestExp = devs.filter(dev =>  dev.yrsExperience >= mostExperience);
-// const mostExp = hasBestExp.map(dev => dev.name)
-// const jobType = hasBestExp.map(dev => dev.profession)
-
-// const studentsWithIds2 = students.map(student => {
-//     for(p in student) {    
-//       if (p == 'name' ) {      
-//         var x = `${p}: ${student[p]}`
-//       }
-//         };
-//     for(g in student) {
-//       if (g == 'id') {      
-//           var y = `${g}: ${student[g]}`
-//         }
-//           }; 
-       
-//       return [x,y]
-//       })
-  
-//   console.log('students with ids 2 =', studentsWithIds2)
-
-// const highScores = student => {
-//     let strongSkills = student.results.filter(skill => skill.english >= 5);
-//     return strongSkills.length > 0;
-//   }
-  
-//   const candidates2 = students.filter( highScores);
-
-//   console.log('hasStrongSkills :',candidates2);
